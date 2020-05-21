10ed0f2036b77b0ae608f3836d812b8e72bc5537
Hugo Tessaro Aledi <hugoaledi@gmail.com>
2020-05-21 18:02:35 -0300

    Criando componente pageheader e utilizando nas páginas

--- todo-app/frontend/src/about/about.jsx
+++ todo-app/frontend/src/about/about.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default (propos) => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name="Sobre" small="Nós" />
+
+    <h2>Nossa História</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod possimus
+      nobis non libero distinctio iusto suscipit quasi eos nulla necessitatibus
+      beatae quisquam sint corporis fugiat tempore consequuntur, minus, unde
+      cupiditate.
+    </p>
+    <h2>Missão e Visão</h2>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio quia ex
+      laborum provident architecto, libero eaque est placeat assumenda alias
+      temporibus. Ipsa officia velit dolores ea quasi veritatis libero dolor!
+    </p>
+    <h2>Imprensa</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
+      inventore vero rem repudiandae. Adipisci at ullam quibusdam voluptates
+      voluptatum eos aliquam minus nesciunt. Sint eligendi ipsam cumque rem
+      quae? Sed.
+    </p>
   </div>
 );
