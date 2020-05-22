c073015f636a91b029f3aff1dcdccf16904c1a11
Hugo Tessaro Aledi <hugoaledi@gmail.com>
2020-05-22 10:53:41 -0300

    Resolvendo erro de cors

--- todo-app/backend/src/config/cors.js
+++ todo-app/backend/src/config/cors.js
@@ -1,11 +1,11 @@
 module.exports = function (req, res, next) {
-  res.header("Access-Controle-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Controle-Allow-Methods",
+    "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   res.header(
-    "Access-Controle-Allow-Headers",
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
